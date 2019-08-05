3accc07dc738a02e2854b225e37dc44d34791f53
MisterHuh <jaehuh86@yahoo.com>
2019-08-05 13:36:18 -0700

    made changes

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 
+const express = require("express"); // loaded the code and stored into express
 
-const express = require("express");
+const server = express();
+
+function handleGetRequestToRoot( request, response ){
+  console.log("/ was called");
+  response.send("Hello, world");
+}
+server.get("/", handleGetRequestToRoot);
+
+const compliments = [
+  "nice to meet you",
+  "you look nice",
+  "you look great",
+  "you look bad",
+  "you look terrible"
+];
+
+server.get("/compliments", function( request, response ){
+  let compLength = compliments.length;
+  let randomizer = Math.floor(Math.random() * compLength);
+  response.send(compliments[randomizer]);
+})
+
+server.listen( 3001, function(){console.log("server has started 3")});
+// 3001 = port;
